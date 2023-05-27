a1effaca17945572c10db5d6ea4aa10a3fe6e4f8
MuhammadMubeen1012 <muhammadmubeen1012@gmail.com>
2023-05-27 17:09:39 +0500

    Add App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import logo from "./logo.svg";
+import AnotherUserParticipation from "./AnotherUserParticipation";
 import "./App.css";
 import PollDisplay from "./PollDisplay";
+import UserParticipation from "./UserParticipation";
 
 function App() {
   return (
     <>
       <h1>Welcome to Polling Portal</h1>
-      <PollDisplay />
+      {/* <PollDisplay /> */}
+      <UserParticipation />
+      <AnotherUserParticipation />
     </>
   );
 }
